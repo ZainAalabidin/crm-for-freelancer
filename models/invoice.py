6117814892f3223a5e63b82f9c07895927ba1d84
ZainAlabidin <zynly9303@gmail.com>
2024-10-19 12:30:01 -0700

    add .gitignore

--- models/invoice.py
+++ models/invoice.py
@@ -5,7 +5,7 @@ class Invoice(models.Model):
     _description = 'Custom CRM for Invoice'
 
     payment_status = fields.Selection([('paid', 'Paid'), ('unpaid', 'Unpaid')], 'Payment Status')
-    project_id = fields.Many2one('project', string='Project Name')
-    client_id = fields.Many2one('client', string='Client Name')
+    project_id = fields.Many2one('project', string='Project Name', required=True)
+    client_id = fields.Many2one('client', string='Client Name', required=True)
     total_amount = fields.Float('Total Amount')
     due_date = fields.Date('Due Date')
