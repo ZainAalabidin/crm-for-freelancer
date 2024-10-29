c4b9a2dce0b549343061291809d923b28194dec6
ZainAlabidin <zynly9303@gmail.com>
2024-10-29 13:27:03 -0700

    Add automated cron job for invoice to check if due date is late or not

--- models/invoice.py
+++ models/invoice.py
@@ -9,9 +9,11 @@ class Invoice(models.Model):
     client_id = fields.Many2one('client', string='Client Name', required=True, related='project_id.client_id')
     total_amount = fields.Float('Total Amount')
     due_date = fields.Date('Due Date')
+    is_late = fields.Boolean('Is Late?')
     active = fields.Boolean(default=True)
 
-    # @api.onchange('project_id')
-    # def _onchange_project_id(self):
-    #     if self.project_id:
-    #         self.client_id = self.project_id.client_id
+    def check_due_date(self):
+        records = self.search([])
+        for record in records:
+            if record.due_date and record.due_date < fields.Date.today():
+                record.is_late = True
